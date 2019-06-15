e27dfeae48c2d14706b2c08efa55d85429f72770
Tu joel jimenez <tonyjoe1110@hotmail.com>
2019-06-15 11:55:21 -0500

    Base Desarrollo

---
+++ server/config/config.js
@@ -0,0 +1,6 @@
+/*
+Puertos
+
+*/
+
+process.env.PORT = process.env.PORT || 3000;
---
+++ server/index.js
@@ -0,0 +1,44 @@
+require('./config/config');
+var express = require('express');
+var app = express();
+
+
+const body_parse = require('body-parser');
+
+
+//middleware
+app.use(body_parse.urlencoded({ extended: false }));
+app.use(body_parse.json());
+
+//URL LOGIN
+/* 
+ *Email
+ *Password  
+ *Generar JWT
+    
+    ** Retornara
+        *Un JWT
+        *Nombre Usuario
+ */
+app.post('/Login', function(req, res) {
+    var data = req.body;
+
+    if (data.email === undefined) {
+        res.status(400).json(
+            data_return = {
+                'exito': false,
+                'mensaje': 'El Email es necesario'
+            })
+
+    } else {
+        res.json({
+            data: data
+        });
+    }
+    //var id = req.params.id;
+
+});
+
+app.listen(process.env.PORT, function() {
+    console.log(`Escuchando puerto ${process.env.PORT}`);
+});
